67b95764b8b609b72c672005a2693307ac0b6d67
Jay-Karia <jay.sanjay.karia@gmail.com>
2024-08-03 11:15:13 +0530

    chore: add new paramter for conversion

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,28 @@ import { npmToYarn } from './npmToYarn'
 import { npmToPnpm } from './npmToPnpm'
 import { npmToBun } from './npmToBun'
 
+import { executorCommands } from './utils'
+
 /**
  * Converts between npm and yarn command
  */
-export default function convert (str: string, to: 'npm' | 'yarn' | 'pnpm' | 'bun'): string {
+export default function convert(str: string, to: 'npm' | 'yarn' | 'pnpm' | 'bun', executor: boolean = false): string {
   if (to === 'npm') {
     return str.replace(/yarn(?: +([^&\n\r]*))?/gm, yarnToNPM)
   } else if (to === 'pnpm') {
+    if (executor) {
+      return str.replace("npx", executorCommands["pnpm"])
+    }
     return str.replace(/npm(?: +([^&\n\r]*))?/gm, npmToPnpm)
   } else if (to === 'bun') {
+    if (executor) {
+      return str.replace("npx", executorCommands["bun"])
+    }
     return str.replace(/npm(?: +([^&\n\r]*))?/gm, npmToBun)
   } else {
+    if (executor) {
+      return str.replace("npx", executorCommands["yarn"])
+    }
     return str.replace(/npm(?: +([^&\n\r]*))?/gm, npmToYarn)
   }
 }
