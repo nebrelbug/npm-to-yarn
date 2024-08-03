a575fe99e40116df919899c415943a2e1aae14f2
Jay-Karia <jay.sanjay.karia@gmail.com>
2024-08-03 11:29:47 +0530

    refactor: index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,22 +9,15 @@ import { executorCommands } from './utils'
  * Converts between npm and yarn command
  */
 export default function convert(str: string, to: 'npm' | 'yarn' | 'pnpm' | 'bun', executor: boolean = false): string {
-  if (to === 'npm') {
+  if (executor) {
+    return str.replace("npx", executorCommands[to])
+  } else if (to === 'npm') {
     return str.replace(/yarn(?: +([^&\n\r]*))?/gm, yarnToNPM)
   } else if (to === 'pnpm') {
-    if (executor) {
-      return str.replace("npx", executorCommands["pnpm"])
-    }
     return str.replace(/npm(?: +([^&\n\r]*))?/gm, npmToPnpm)
   } else if (to === 'bun') {
-    if (executor) {
-      return str.replace("npx", executorCommands["bun"])
-    }
     return str.replace(/npm(?: +([^&\n\r]*))?/gm, npmToBun)
   } else {
-    if (executor) {
-      return str.replace("npx", executorCommands["yarn"])
-    }
     return str.replace(/npm(?: +([^&\n\r]*))?/gm, npmToYarn)
   }
 }
