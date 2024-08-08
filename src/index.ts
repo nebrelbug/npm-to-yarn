03e1339f3e5044e23b268dd6bdcc7f6bcf5357ed
Jay-Karia <jay.sanjay.karia@gmail.com>
2024-08-08 10:09:55 +0530

    fix: linting

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { executorCommands } from './utils'
 /**
  * Converts between npm and yarn command
  */
-export default function convert(str: string, to: 'npm' | 'yarn' | 'pnpm' | 'bun', executor: boolean = false): string {
+export default function convert(str: string, to: 'npm' | 'yarn' | 'pnpm' | 'bun', executor = false): string {
   if (executor) {
     return str.replace("npx", executorCommands[to])
   } else if (to === 'npm') {
